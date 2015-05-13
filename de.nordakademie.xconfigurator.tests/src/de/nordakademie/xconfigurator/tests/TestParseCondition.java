a3227595134d7857a9ca4acffe3d6b28d6a98788
Niklas <niklas.rothe@nordakademie.de>
2015-05-13 23:11:30 +0200

    Test

---
+++ de.nordakademie.xconfigurator.tests/src/de/nordakademie/xconfigurator/tests/TestParseCondition.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.nordakademie.xconfigurator.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Bärbel
+ *
+ */
+public class TestParseCondition {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
